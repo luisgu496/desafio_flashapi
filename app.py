4063e7150466673a7659af06bd1bda2c756304e2
Luis Leite <127146111+luisgu496@users.noreply.github.com>
2024-07-12 13:30:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,1 @@
-from fastapi import FastAPI
-from workout_DIO.routers import api_router
-from fastapi_pagination import add_pagination
 
-
-app = FastAPI(title="WokroutAPI-DIO")
-app.include_router(api_router)
-add_pagination(app)
-
-
-@app.get("/")
-def get():
-    return{
-            "endpoint": [route.path for route in app.routes],
-        }
